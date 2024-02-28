0a0dec4bfd9dc432db50f857eb5eb58c8580b75f
권나영 <naa0@lguplus.co.kr>
2024-02-28 20:12:37 +0900

    Update main.py

--- ca_bot/main.py
+++ ca_bot/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import os
+import random  # random 모듈 추가
 
 app = Flask(__name__)
 
@@ -13,14 +14,12 @@ def slack_events():
     data = request.json
     trigger_words = ['선임', '책임', '팀장', '담당', '상무', '전무']
 
-    # 메시지 이벤트 처리
     if data['event']['type'] == 'message' and 'text' in data['event']:
         text = data['event']['text']
         channel_id = data['event']['channel']
         user_id = data['event']['user']
         ts = data['event']['ts']  # 메시지의 timestamp
 
-        # CA 트리거
         if text in ['CA', 'ca', 'CA!', 'ca!']:
             try:
                 response = client.chat_postMessage(
@@ -29,13 +28,20 @@ def slack_events():
                 )
             except SlackApiError as e:
                 print(f"Error posting message: {e}")
-        # 호칭 트리거
         elif any(word in text for word in trigger_words):
+            # 랜덤 메시지 목록
+            random_messages = [
+                f"<@{user_id}>님, 이러시면 안돼요! :춘식눈물:\n님 호칭 사용을 실천해주세요 :루피하트:",
+                f"이름에 님을 안 붙였네요? 오늘 저와 단둘이 저녁식사하시죠~:superman_cto:",
+                "봄날같은 인사, ‘님’과 함께 시작해보세요!"
+            ]
             try:
+                # 랜덤 메시지 선택
+                random_message = random.choice(random_messages)
                 response = client.chat_postMessage(
                     channel=channel_id,
-                    text=f"<@{user_id}>님, 이러시면 안돼요! :춘식눈물:\n님 호칭 사용을 실천해주세요 :루피하트:",  # 멘트
-                    thread_ts=ts # 이 메시지를 스레드로 연결
+                    text=random_message,
+                    thread_ts=ts  # 이 메시지를 스레드로 연결
                 )
             except SlackApiError as e:
                 print(f"Error posting message: {e}")
