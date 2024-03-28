d0d72c316bb9d57f310260edfd8883784cb5c2c0
권나영 <naa0@lguplus.co.kr>
2024-03-28 20:43:48 +0900

    Update main.py

--- ca_bot/main.py
+++ ca_bot/main.py
@@ -11,6 +11,26 @@ app = Flask(__name__)
 SLACK_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 client = WebClient(token=SLACK_TOKEN)
 
+def get_secret(secret_name):
+    """Google Cloud Secret Manager에서 Secret 값을 조회합니다."""
+    client = secretmanager.SecretManagerServiceClient()
+    project_id = "main-nucleus-413512"  # 여기에 실제 Google Cloud 프로젝트 ID를 입력하세요.
+    name = f"projects/627744097884/secrets/GOOGLE_APPLICATION_CREDENTIALS_JSON/latest"
+    response = client.access_secret_version(request={"name": name})
+    secret_string = response.payload.data.decode("UTF-8")
+    return secret_string
+
+def initialize_gspread():
+    """Secret Manager에서 조회한 서비스 계정 키를 사용하여 gspread 클라이언트를 초기화합니다."""
+    secret_name = "GOOGLE_APPLICATION_CREDENTIALS_JSON"
+    service_account_info = get_secret(secret_name)
+    credentials_dict = json.loads(service_account_info)
+    credentials = Credentials.from_service_account_info(credentials_dict)
+    gc = gspread.authorize(credentials)
+    return gc
+
+gc = initialize_gspread()  # gspread 클라이언트 초기화
+
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
     data = request.json
