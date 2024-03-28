451e3d8658df465be42b63f5d07e678c071eddd8
권나영 <naa0@lguplus.co.kr>
2024-03-28 22:18:43 +0900

    Update main.py

--- ca_bot/main.py
+++ ca_bot/main.py
@@ -39,38 +39,44 @@ def slack_events():
                 )
             except SlackApiError as e:
                 print(f"Error posting message: {e}")
-        elif re.search(trigger_words_pattern, text) and not (user_name == "caca"): 
-            
-            # 채널 정보 가져오기
-            channel_info = client.conversations_info(channel=channel_id)
-            channel_name = channel_info['channel']['name']
-                
-            # 랜덤 메시지 목록
-            random_messages = [
-                f"<@{user_id}>님, 이러시면 안돼요! :춘식눈물:\n님 호칭 사용을 실천해주세요 :루피하트:",
-                "담당님 테스트중"
-            ]
-            try:
-                # 랜덤 메시지 선택
-                random_message = random.choice(random_messages)
-                response = client.chat_postMessage(
-                    channel=channel_id,
-                    text=random_message,
-                    thread_ts=ts  # 이 메시지를 스레드로 연결
-                )
+        else:
+            triggered_words = []
+            for word in trigger_words:
+                if word in text:
+                    triggered_words.append(word)
 
-                # Firestore에 데이터 저장
-                doc_ref = db.collection(u'slack_events').document()
-                doc_ref.set({
-                    u'user_id': user_id,
-                    u'user_name': user_name,  # 사용자 이름 저장
-                    u'timestamp': ts,
-                    u'text': text,
-                    u'channel_id': channel_id,
-                    u'channel_name': channel_name  # 채널 이름 저장
-                })
-            except SlackApiError as e:
-                print(f"Error posting message: {e}")
+            if triggered_words and not (user_name == "caca"): 
+                # 채널 정보 가져오기
+                channel_info = client.conversations_info(channel=channel_id)
+                channel_name = channel_info['channel']['name']
+                    
+                # 랜덤 메시지 목록
+                random_messages = [
+                    f"<@{user_id}>님, 이러시면 안돼요! :춘식눈물:\n님 호칭 사용을 실천해주세요 :루피하트:",
+                    "담당님 테스트중"
+                ]
+                try:
+                    # 랜덤 메시지 선택
+                    random_message = random.choice(random_messages)
+                    response = client.chat_postMessage(
+                        channel=channel_id,
+                        text=random_message,
+                        thread_ts=ts  # 이 메시지를 스레드로 연결
+                    )
+
+                    # Firestore에 데이터 저장
+                    doc_ref = db.collection(u'slack_events').document()
+                    doc_ref.set({
+                        u'user_id': user_id,
+                        u'user_name': user_name,  # 사용자 이름 저장
+                        u'timestamp': ts,
+                        u'text': text,
+                        u'channel_id': channel_id,
+                        u'channel_name': channel_name,  # 채널 이름 저장
+                        u'triggered_words': triggered_words  # 트리거된 단어 저장
+                    })
+                except SlackApiError as e:
+                    print(f"Error posting message: {e}")
 
     return '', 200
 
