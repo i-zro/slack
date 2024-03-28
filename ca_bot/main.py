b9129e3ac33f6eff9df6e432a89307bf4848048e
권나영 <naa0@lguplus.co.kr>
2024-03-28 23:45:56 +0900

    Update main.py

--- ca_bot/main.py
+++ ca_bot/main.py
@@ -37,6 +37,8 @@ def process_event(data):
         except SlackApiError as e:
             print(f"Error posting message: {e}")
     else:
+        triggered_words = [word for word in trigger_words if word in text]
+
         # '님들'로 끝나는 단어를 triggered_words에서 제거
         triggered_words = [word for word in triggered_words if not word.endswith('님들')]
     
